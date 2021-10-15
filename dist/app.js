1afa4ececd1b497834bec722d604ee79ed2735e7
guillermoandrae <guillermo@joinhandshake.com>
2021-10-15 09:01:05 -0400

    Scope?

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,7 @@
-const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/random";
-const GIPHY_API_KEY = "PZOC6JcBSsuGNg71Ct9IsBrQ7sBRv7jK";
-
 $( function () {
-    $.getJSON( GIPHY_API_URL, { "api_key": GIPHY_API_KEY, "tag": "trash" }, function( response ) {
+    const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/random";
+    const GIPHY_API_KEY = "PZOC6JcBSsuGNg71Ct9IsBrQ7sBRv7jK";
+    $.getJSON( GIPHY_API_URL, { "api_key": GIPHY_API_KEY, "tag": "trash", "rating": "g" }, function( response ) {
         const data = response.data;
         let giphy = $("<img />", {
             "src": data.images.original.url,
