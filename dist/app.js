2fbf269e9f673ef6429fed0ad22c1dceaba8d3b6
guillermoandrae <guillermo@joinhandshake.com>
2021-10-15 00:13:53 -0400

    Initial commit.

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/random";
+const GIPHY_API_KEY = "PZOC6JcBSsuGNg71Ct9IsBrQ7sBRv7jK";
+
+$( function () {
+    $.getJSON( GIPHY_API_URL, { "api_key": GIPHY_API_KEY, "tag": "trash" }, function( response ) {
+        const data = response.data;
+        let giphy = $("<img />", {
+            "src": data.images.original.url,
+            "alt": data.title
+        });
+        $("#gif").append(giphy);
+    });
+});
