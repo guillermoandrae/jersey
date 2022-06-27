a0592b3c8c5f937f2ce3f3990d4c342b354758aa
guillermoandrae <guillermoandraefisher@gmail.com>
2022-06-27 11:34:49 -0400

    Fixing the API call.

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,7 @@
 $( function () {
     $.getJSON( "https://api.guillermoandraefisher.com/jersey", function( response ) {
         const data = response.data;
-        let giphy = $("<img />", {
-            "src": data.images.original.url,
-            "alt": data.title
-        });
+        let giphy = $("<img />", data);
         $("#gif").append(giphy);
     });
 });
