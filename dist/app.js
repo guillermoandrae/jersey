5db86e42246d79ac88478fe880ccf3b24a4906a5
guillermoandrae <guillermoandraefisher@gmail.com>
2022-06-27 11:30:47 -0400

    API!

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,5 @@
 $( function () {
-    const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/random";
-    const GIPHY_API_KEY = "PZOC6JcBSsuGNg71Ct9IsBrQ7sBRv7jK";
-    $.getJSON( GIPHY_API_URL, { "api_key": GIPHY_API_KEY, "tag": "trash", "rating": "g" }, function( response ) {
+    $.getJSON( "https://api.guillermoandraefisher.com/jersey", function( response ) {
         const data = response.data;
         let giphy = $("<img />", {
             "src": data.images.original.url,
